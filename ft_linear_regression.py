b706c3335b4d5484555ae61090f661b29ba474cc
Jacquet Thomas <cjacquet@student.42.fr>
2021-04-05 19:55:06 +0200

    change for to while for regression and add show-all option

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -6,16 +6,16 @@ import tools as t
 def estimate_price(theta_0=0.0, theta_1=0.0, km=0):
     return theta_0 + (theta_1 * km)
 
-def train(display=False):
+def train(display=False, display_all=False):
     learning_ratio=0.3
-    theta_0 = 0.
-    theta_1 = 0.
+    theta_0, theta_1 = 0., 0.
+    delta_0, delta_1 = 1., 1.
     data = t.get_data()
     scaleddata = t.scale(data)
     m = len(data)
     if m == 0:
         return
-    
+   
    # Draw base graph 
     if display:
         for dat in data:
@@ -25,7 +25,8 @@ def train(display=False):
         plt.suptitle("cloud points of price/km")
         line, = plt.plot([250000, 100], [estimate_price(theta_0, theta_1, 250000), estimate_price(theta_0, theta_1, 100)], '-r')
 
-    for i in range(m**3):
+    i = 0
+    while abs(delta_0) > 0.001 and abs(delta_1) > 0.001:
         sum_0 = 0.
         sum_1 = 0.
         print("i = {} theta_0={} theta_1={}".format(i, theta_0, theta_1))
@@ -39,16 +40,18 @@ def train(display=False):
         print("delta_0={} delta_1={}\n".format(delta_0, delta_1))
         theta_0 -= delta_0
         theta_1 -= delta_1
-
+        i += 1
         # Update linear regression line
-        if display:
+        if display_all:
             line.set_ydata([estimate_price(theta_0, theta_1 / max_km, 250000), estimate_price(theta_0, theta_1 / max_km, 100)])
-            plt.pause(0.0001)
+            plt.pause(0.00001)
+
     theta_1 /= (max_km)
     print("----------------------------\ntheta_0={} theta_1={}".format(theta_0, theta_1))
     t.save_thetas(theta_0, theta_1)
     if display:
-        plt.show()
+        line.set_ydata([estimate_price(theta_0, theta_1, 250000), estimate_price(theta_0, theta_1, 100)])
+        plt.show(block = True)
 
 def display_graph():
     
@@ -57,6 +60,13 @@ def display_graph():
     plt.show()
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "-s":
-        train(True)
-    train()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--show":
+            train(True, False)
+        elif sys.argv[1] == "--show-all":
+            train(True, True)
+        else:
+            print("unknown option, basic training will run")
+            train()
+    else:
+        train()
